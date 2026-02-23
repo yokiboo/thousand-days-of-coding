0328e460d0e16eed30d481dacf443c5d554238e4
unknown <yoheshwar1992@gmail.com>
2026-02-23 23:53:43 +0530

    date_23-02-2026

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.dailyPractice;
+
+public class Employee {
+	
+	public Employee(String name) {
+		this.name = name;
+	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
